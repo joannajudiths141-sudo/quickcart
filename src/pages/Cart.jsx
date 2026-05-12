283842a3897a88184b315f3d6e6bcd21e40f39ff
Joanna <yourgithubemail@gmail.com>
2026-05-12 23:53:31 +0530

    Completed QuickCart Part 3 features

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
+function Cart() {
+  const { cart } = useContext(CartContext);
+
+  return (
+    <div>
+      <h1>Cart</h1>
+
+      {cart.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        cart.map((item, index) => (
+          <div key={index}>
+            <h3>{item.title}</h3>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default Cart;
